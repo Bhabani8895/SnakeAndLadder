69490401314018eb9c10ebc48a70969137c30667
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-01-31 23:22:43 +0530

    [Bhabani] rolling the dice

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,8 +7,12 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder");
-            int Position = 0;
-            Console.WriteLine("Player is at starting  Position: " + Position);
+            //int Position = 0;
+            //Console.WriteLine("Player is at starting  Position: " + Position);
+            int player1 = 0;
+            Random random = new Random();
+            player1 = random.Next(1, 6);
+            Console.WriteLine(player1);
         }
     }
 }

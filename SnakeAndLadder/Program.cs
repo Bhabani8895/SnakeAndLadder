bd63dcb2ed549d0c3e398c2bc5af1e24a3ca6472
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-01-31 23:38:38 +0530

    [Bhabani] Reaching Winning Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,35 +7,40 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder");
-            //int Position = 0;
-            //Console.WriteLine("Player is at starting  Position: " + Position);
-            //int player1 = 0;
-            //Random random = new Random();
-            //player1 = random.Next(1, 6);
-            //Console.WriteLine(player1);
             const int NO_PLAY = 1;
             const int LADDER = 2;
             const int SNAKE_BITE = 3;
             int position = 0;
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            int checkOption = random.Next(1, 4);
-            switch (checkOption)
+
+            while (position <= 100)
             {
-                case NO_PLAY:
-                    position = 0;
-                    break;
-                case LADDER:
-                    position = position + dice;
-                    break;
-                case SNAKE_BITE:
-                    position = position - dice;
-                    break;
-                default:
-                    position = 0;
-                    break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                int checkOption = random.Next(1, 4);
+                switch (checkOption)
+                {
+                    case NO_PLAY:
+                        position = 0;
+                        break;
+                    case LADDER:
+                        position = position + dice;
+                        break;
+                    case SNAKE_BITE:
+                        position = position - dice;
+                        break;
+                    default:
+                        position = 0;
+                        break;
+                }
+                if (position < 0)
+                {
+                    Console.WriteLine("Player restarts from 0");
+                }
+                else
+                {
+                    Console.WriteLine("Player stays in same postion");
+                }
             }
-            Console.WriteLine("postion of player is :" + position);
         }
     }
 }

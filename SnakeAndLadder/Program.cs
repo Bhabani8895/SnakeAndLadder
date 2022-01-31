a8e41a6e65da5a8c33b3203120b73877d2a90512
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-01-31 23:30:39 +0530

    [Bhabani] option check

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,10 +9,33 @@ namespace SnakeAndLadder
             Console.WriteLine("Welcome to Snake and Ladder");
             //int Position = 0;
             //Console.WriteLine("Player is at starting  Position: " + Position);
-            int player1 = 0;
+            //int player1 = 0;
+            //Random random = new Random();
+            //player1 = random.Next(1, 6);
+            //Console.WriteLine(player1);
+            const int NO_PLAY = 1;
+            const int LADDER = 2;
+            const int SNAKE_BITE = 3;
+            int position = 0;
             Random random = new Random();
-            player1 = random.Next(1, 6);
-            Console.WriteLine(player1);
+            int dice = random.Next(1, 7);
+            int checkOption = random.Next(1, 4);
+            switch (checkOption)
+            {
+                case NO_PLAY:
+                    position = 0;
+                    break;
+                case LADDER:
+                    position = position + dice;
+                    break;
+                case SNAKE_BITE:
+                    position = position - dice;
+                    break;
+                default:
+                    position = 0;
+                    break;
+            }
+            Console.WriteLine("postion of player is :" + position);
         }
     }
 }

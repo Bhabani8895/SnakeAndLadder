2c6831dfa93464522112c2eddaafb06f6c63ee34
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-01-31 23:04:00 +0530

    [Bhabani] Initial Player Position Added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,7 +7,8 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder");
-
+            int Position = 0;
+            Console.WriteLine("Player is at starting  Position: " + Position);
         }
     }
 }
